f46bcce9204a46639bfc223cc2b2ba3bb4f1ad7b
shiva123kphkp <91113459+shiva123kphkp@users.noreply.github.com>
2021-09-30 12:38:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🎖️ our group 🎖️', url="https://t.me/mallumoviesp"
                                 )
                         ]
                     ]
